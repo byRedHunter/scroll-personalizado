0a2a18882b6fa6c7268f4f2a5bd9292442b536f2
Jhonny Quispe <55221373+byRedHunter@users.noreply.github.com>
2020-03-22 08:05:09 -0500

    Delete archivo preLoader.js

--- js/preLoader.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    //$('#onload').fadeOut();
-    let $body = document.querySelector('#body')
-    let $loader = document.querySelector('#pre-loader')
-
-    $body.classList.remove('hidden')
-    $loader.classList.add('ocultarLoader')
-    $loader.style.display = 'none'
-}
